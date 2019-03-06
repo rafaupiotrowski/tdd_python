a5a57af6fbae3f06be9b1d879f189d97c7b109de
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-03-06 12:57:51 +0100

    Test funkcjonalny teraz sprawdza możliwość dodania elementu listy

---
+++ functional_tests.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+	def setUp(self):
+		self.browser = webdriver.Chrome()
+		self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_start_a_list_and_retrive_it_later(self):
+		#Edyta dowiedziała się o nowej, wspaniałej aplikacji w postaci listy rzeczy do zrobienia.
+		#Postanowiła więc przejść na stronę główną tej aplikacji.
+		self.browser.get('http://localhost:8000')
+
+		#Zwróciła uwagę, że tytuł strony i nagłówek zawierają słowo Listy.
+		self.assertIn('Listy', self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Listy', header_text)
+
+		#Od razu zostaje zachęcona, aby wpisać rzecz do zrobienia.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Wpisz rzecz do zrobienia'
+		)
+
+		#W polu tekstowym wpisuje 'Kupić pawie pióra' (hobby Edyty
+		#polega na tworzeniu ozdobnych przynęt).
+		inputbox.send_keys('Kupić pawie pióra')
+
+		#Po naciśnięciu klawisza Enter strona została uaktulaniona i wyświetla
+		#'1: Kupić pawie pióra' jako element listy rzeczy do zrobienia.
+		inputbox.send_keys(Keys.Enter)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue( any(row.text == '1: Kupić pawie pióra' for row in rows))
+
+		#Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
+		#Edyta wpisała 'Użyć pawich piór do zrobienia przynęty' (Edyta jest niezwykle skrupulatna).
+		self.fail('Zakończenie testu!')
+		#Strona została ponownie uaktualniona i teraz wyświetla dwa elelementy na liście rzeczy do zrobienia.
+
+		#Edyta była ciekwawa, czy witryna zapamięta jej listę. Zwróciła uwagę na wygenerowany dla niej unikatowy adres URL, obok którego znajduje się pewien tekst z wyjaśnieniem
+
+		#Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia.
+
+		#Usatysfakcjonowana kładzie się spać.
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')

38ec547ae90a511ef4f0275eb282719fee4474f6
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-03-10 21:39:55 +0100

    Refaktoryzacja -nowy plik urls.py w katalogu lists

--- lists/urls.py
+++ lists/urls.py
@@ -19,6 +19,7 @@ from lists import views
 
 urlpatterns = [
 #    path('admin/', admin.site.urls),
-	path('', views.home_page, name='home'),
-	path('lists/', include('lists.urls')),
+	path('<str:list_id>/', views.view_list, name='view_list'),
+	path('new', views.new_list, name = 'new_list'),
+	path('<str:list_id>/add_item', views.add_item, name = 'add_item'),
 ]

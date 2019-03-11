d9fb4477a6efb5334cc77e47af04c55c0d32b8cb
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-03-11 21:07:10 +0100

    Pierwsze kroki w teście funkcjonalnym dla układu i stylów.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -87,8 +87,31 @@ class NewVisitorTest(LiveServerTestCase):
 
 		#Usatysfakcjonowani, oboje kładą się spać.
 		
-		self.fail('Zakończenie testu!')
 
 		#Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia.
 
 		#Usatysfakcjonowana kładzie się spać.
+
+	def test_layout_and_styling(self):
+		#Edyta przeszła na stronę główną
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+		
+		#Zauważyła, że pole tekstowe zostało elegancko wyśrodkowane.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+		inputbox.location['x'] + inputbox.size['width']/2,
+		512,
+		delta=5
+		)
+		
+		#Edyta utworzyła nową listę i zobaczyła, że pole tekstowe nadal jest wyśrodkowane.
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+		inputbox.location['x'] + inputbox.size['width']/2,
+		512,
+		delta=5
+		)
+
+		self.fail('Zakończenie testu!')

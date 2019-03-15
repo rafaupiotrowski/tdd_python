45f4c430f54a575dbb5bfc00a466f33eb0b9997a
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-03-15 12:31:28 +0100

    Przeniesienie bazy danych sqlite poza gałąź głównego kodu żródłowego.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,17 +10,17 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	@classmethod
 	def setUpClass(cls):
-		for arg in sys.argv:
-			if 'liveserver' in arg:
-				cls.server_url = 'http://' +arg.split('=')[1]
-				return
-		super().setUpClass()
-		cls.server_url = cls.live_server_url
+	#	for arg in sys.argv:
+	#		if 'liveserver' in arg:
+	#			cls.server_url = 'http://' +arg.split('=')[1]
+	#			return
+	#	super().setUpClass()
+		cls.server_url = 'http://rafalpiotrowski.com.pl'
 
-	@classmethod
-	def tearDownClass(cls):
-		if cls.server_url ==cls.live_server_url:
-			super().tearDownClass()
+	#@classmethod
+	#def tearDownClass(cls):
+	#	if cls.server_url ==cls.live_server_url:
+	#		super().tearDownClass()
 
 	def setUp(self):
 		self.browser = webdriver.Chrome()

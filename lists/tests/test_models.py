0dddbaecc93fb6aa33dc0610456ce2cee8c2e85a
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-04-04 14:06:45 +0200

    Umieszczenie testów jednostkowych w dwóch oddzielnych plikach.

---
+++ lists/tests/test_models.py
@@ -0,0 +1,37 @@
+from lists.models import Item, List
+from lists.views import home_page
+from django.urls import resolve
+from django.test import TestCase, Client
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
+from lists.views import home_page
+
+class ListandItemModelsTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
+		first_item = Item()
+		first_item.text = 'Absolutnie pierwszy element listy'
+		first_item.list = list_
+		first_item.save()
+		
+		second_item = Item()
+		second_item.text = 'Drugi element'
+		second_item.list = list_
+		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(),2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'Absolutnie pierwszy element listy')
+		self.assertEqual(second_saved_item.text, 'Drugi element')
+		self.assertEqual(first_saved_item.list, list_)
+		self.assertEqual(second_saved_item.list, list_)

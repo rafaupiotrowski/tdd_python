d2e8426fbc8e437cbd4f3d66575b1860889a6be2
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-04-04 13:57:26 +0200

    Rozwiniecie test_list_item_validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,22 +11,33 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 class ItemValidationTest(FunctionalTest):
+	
+    def test_cannot_add_empty_list_items(self):
+        #Edyta przeszła na stronę główną i przypadkowo spróbowała
+        # utworzyć pusty element na liście. Nacisnęła enter w pustym polu
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-		
-	def test_cannot_add_empty_list_items(self):
-		#Edyta przeszła na stronę główną i przypadkowo spróbowała
-		# utworzyć pusty element na liście. Nacisnęła enter w pustym polu
-
-		#Po odświeżeniu strony głównej zobaczyła komunikat błędu
+        #Po odświeżeniu strony głównej zobaczyła komunikat błędu
+        error = self.browser.find_element_by_css_selector('.haserror')
+        self.assertEqual(error.text, 'Element nie może być pusty')
 
 		#Spróbowała ponownie, wpisując tekst, i wszystko zadziałało
+        self.browser.find_element_by_id('id_new_item').send_keys('Kupić mleko\n')
+        self.check_for_row_in_list_table('1: Kupić mleko')
 
 		#Przekornie drugi raz spróbowała utworzyć pusty element na liście
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-		#Na stronei listy otzymała komuninkat jak wcześniej
+		#Na stronie listy otzymała komuninkat jak wcześniej
+        self.check_for_row_in_list_table('1: Kupić mleko')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, 'Element nie może być pusty')
 
 		#Element mogła poprawić, wpisując w nim dowolny tekst.
-		self.fail('Napisz mnie')
+        self.browser.find_element_by_id('id_new_item').send_keys('Kupić mleko\n')
+        self.fail('Napisz mnie')
+        self.check_for_row_in_list_table('1: Kupić mleko')
+        self.check_for_row_in_list_table('2: Zrobić herbatę')
 
-#		self.fail('Zakończenie testu!')
 

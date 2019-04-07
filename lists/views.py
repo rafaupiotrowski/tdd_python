a61033387f5e96564019ada3c68af24413497dce
Rafal Piotrowski <rafal.piotrowski@o2.pl>
2019-04-07 13:52:52 +0200

    Wykorzystanie znacznika szablonu url

--- lists/views.py
+++ lists/views.py
@@ -34,10 +34,6 @@ def new_list(request):
 		return render(request, 'home.html', {'error': error})
 	return redirect('view_list', list_.id)
 
-def add_item(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/%d/' % (list_.id))
 
 
 
